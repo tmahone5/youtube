936b426ad8ac81339e20159c44f07a3490bb3e4c
mi9l <tremaynemahone@navapbc.com>
2025-10-21 11:30:53 -0500

    adding function to gather video ID'd

--- video_stats.py
+++ video_stats.py
@@ -7,8 +7,9 @@ load_dotenv(dotenv_path="./.env")
 
 # Global variables use in url f string
 API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = "Francois-B-Arthanas"
-
+CHANNEL_HANDLE = "MrBeast"
+maxResults = 50
+# Function to get playlist ID, which will be used to get video ids
 def get_playlist_id():
     
     try:
@@ -25,14 +26,48 @@ def get_playlist_id():
         # uses json dumps method, using data var and indent 4 spaces
         #print(json.dumps(data, indent=4))
 
-        #parses json dump
+        # parses json dump
         channel_items = data['items'][0]
         channel_playlistId = channel_items['contentDetails']['relatedPlaylists']['uploads']
-        print(channel_playlistId)
+        #print(channel_playlistId)
         return channel_playlistId
         
     except requests.exceptions.RequestException as e:
         raise (e)
+
+# Function to get video IDs tha will be iterated through
+def get_video_ids(playlistId):
+    
+    video_ids = []
+    pageToken = None
+    base_url = f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}'
+
+    try:
+        while True:
+            url = base_url
+            if pageToken:
+                url += f'&pageToken={pageToken}'
+            response = requests.get(url)
+            
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            pageToken = data.get('nextPageToken')
+
+            if not pageToken:
+                break
+
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
         
 if __name__ == "__main__":
-    get_playlist_id()
+    playlistId = get_playlist_id()
+    get_video_ids(playlistId)
